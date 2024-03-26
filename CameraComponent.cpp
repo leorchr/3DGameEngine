49d04eaf323eaa248c0f102a65ac55e9ee47776a
L3o <leogravillon@hotmail.fr>
2024-03-26 12:10:23 +0100

    Initial commit

---
+++ CameraComponent.cpp
@@ -0,0 +1,12 @@
+#include "CameraComponent.h"
+#include "Game.h"
+
+CameraComponent::CameraComponent(Actor* owner, int updateOrder) : Component(owner, updateOrder)
+{
+}
+
+void CameraComponent::setViewMatrix(const Matrix4& view)
+{
+	Game& game = owner.getGame();
+	game.getRenderer().setViewMatrix(view);
+}
